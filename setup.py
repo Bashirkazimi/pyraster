6ebfc231d464d71cf2a9cdf09ccb267ec9e1a290
Bashir Kazimi <kazimibashir907@gmail.com>
2021-03-04 09:17:06 +0100

    testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.4'
+VERSION = '0.1.5'
 PACKAGE_NAME = 'pyraster'
 AUTHOR = 'Bashir Kazimi'
 AUTHOR_EMAIL = 'kazimibashir907@gmail.com'
